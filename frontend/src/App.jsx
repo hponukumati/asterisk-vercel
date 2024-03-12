fde64c7a102d3c206020af7fe2c86e754e7ec48e
hponukumati <hponukumati@gmail.com>
2024-03-12 13:29:57 -0700

    vercel v5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -43,7 +43,7 @@ function App() {
       <h1>Things to do if you are bored</h1>
       <p>When you click the button you might find a lot of things that you can do some of which can be adventurous and others which can be very chill.</p>
       <button onClick={handleRandomSelect} disabled={isButtonDisabled}>
-        {isButtonDisabled ? "🎉 Adventure unlocked! You've experienced all the fun we've got... for now. Hang tight – the next fun drop is on the horizon! 🌟" : 'Find Your Fun'}
+        {isButtonDisabled ? "🎉 Adventure unlocked! You've experienced all the fun we've got... for now. Hang tight – the next fun drop is on the horizon! 🌟" : "Find Your Fun"}
       </button>
       {randomItem && (
         <div>
